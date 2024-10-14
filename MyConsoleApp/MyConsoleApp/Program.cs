40b7b12bf2284af9b921e2dfb6b3ad1fed72e398
Abdelrahman Elmagry <124198691+elmagry123@users.noreply.github.com>
2024-10-14 21:54:17 +0300

    Add files via upload

---
+++ MyConsoleApp/MyConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyLibrary;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            int result = calc.Add(5, 3);
+            Console.WriteLine("The result is: " + result);
+        }
+    }
+}
